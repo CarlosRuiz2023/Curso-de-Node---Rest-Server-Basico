7536e3e671ceeabc89122cb3945354b5621b5e66
Juan Carlos Ruiz Gomez <juancarlosruizgomez2000@gmail.com>
2023-10-29 23:16:03 -0600

    JWT implementado

--- models/server.js
+++ models/server.js
@@ -6,13 +6,11 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
-
+    this.authPath = "/api/auth";
     //Conectar a base de datos
     this.conectarDB();
-
     //Middlewares
     this.middlewares();
-
     //Rutas de mi aplicacion
     this.routes();
   }
@@ -31,6 +29,7 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usuariosPath, require("../routes/usuarios"));
   }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,13 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { validarCampos } = require("../middlewares/validar-campos");
+const {
+  validarCampos,
+  validarJWT,
+  esAdminRole,
+  tieneRole,
+} = require("../middlewares");
+
 const {
   esRoleValido,
   emailExiste,
@@ -50,6 +56,9 @@ router.post(
 router.delete(
   "/:id",
   [
+    validarJWT,
+    //esAdminRole,
+    tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
     check("id", "No es un ID Valido").isMongoId(),
     check("id").custom(existeUsuarioPorId),
     validarCampos,
