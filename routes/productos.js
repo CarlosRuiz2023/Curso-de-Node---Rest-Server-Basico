4b6e23748409ad8e68c4a550a33a6bc05953cf05
Juan Carlos Ruiz Gomez <juancarlosruizgomez2000@gmail.com>
2023-10-31 10:20:13 -0600

    CRUD Productos, categorias y busqueda

---
+++ routes/productos.js
@@ -0,0 +1,75 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { validarJWT, validarCampos, esAdminRole } = require("../middlewares");
+const {
+  crearProducto,
+  obtenerProductos,
+  obtenerProducto,
+  actualizarProducto,
+  borrarProducto,
+} = require("../controllers/productos");
+const {
+  existeCategoriaPorId,
+  existeProductoPorId,
+} = require("../helpers/db-validators");
+
+const router = Router();
+
+/**
+ * {{url}}/api/categorias
+ */
+
+//Obtener todas las categorias - publico
+router.get("/", obtenerProductos);
+
+//Obtener una categoria por id - publico
+router.get(
+  "/:id",
+  [
+    //check("id", "No es un id de Mongo valido").isMongoId(),
+    check("id").custom(existeProductoPorId),
+    validarCampos,
+  ],
+  obtenerProducto
+);
+
+//Crear categoria - privado - cualquier persona con unn token valido
+router.post(
+  "/",
+  [
+    validarJWT,
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("categoria", "No es un id de Mongo").isMongoId(),
+    check("categoria").custom(existeCategoriaPorId),
+    validarCampos,
+  ],
+  crearProducto
+);
+
+//Actualizar - privado - cualquiera con token valido
+router.put(
+  "/:id",
+  [
+    validarJWT,
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("id").custom(existeProductoPorId),
+    validarCampos,
+  ],
+  actualizarProducto
+);
+
+//Borrar una categoria - Admin
+router.delete(
+  "/:id",
+  [
+    validarJWT,
+    esAdminRole,
+    check("id", "No es un id de Mongo valido").isMongoId(),
+    check("id").custom(existeProductoPorId),
+    validarCampos,
+  ],
+  borrarProducto
+);
+
+module.exports = router;
